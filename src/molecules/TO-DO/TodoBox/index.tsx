1a6a60eca721eb49ea4cea6d4c2b653f6998730c
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-01-16 13:16:01 +0900

    Feat: Todo 레이아웃

--- src/molecules/TO-DO/TodoBox/index.tsx
+++ src/molecules/TO-DO/TodoBox/index.tsx
@@ -1,8 +1,25 @@
+import styled from "styled-components";
+
+const TodoMainBox = styled.div`
+  border-radius: 16px;
+  width: 25vw;
+  height: 10vh;
+  border: 0.5px solid #c8c5cb;
+  box-shadow: 1px 1px 1px 1px #c8c5cb;
+`;
+
+const TodoRecentHeaderText = styled.h3`
+  font-family: "Pretendard";
+  font-weight: 700;
+  font-size: 20px;
+`;
+
 const TodoBox = () => {
   return (
-    <section>
-      <div>투두박스입니다</div>
-    </section>
+    <article>
+      <TodoRecentHeaderText>지난 일정</TodoRecentHeaderText>
+      <TodoMainBox>투두박스입니다</TodoMainBox>
+    </article>
   );
 };
 
--- src/organisms/TodoList/index.tsx
+++ src/organisms/TodoList/index.tsx
@@ -1,10 +1,18 @@
 import TodoBox from "@/molecules/TO-DO/TodoBox";
+import styled from "styled-components";
+
+const TodoListMainBox = styled.section`
+  width: 25vw;
+  height: 100vh;
+  margin-top: 57px;
+  margin-left: 67px;
+`;
 
 const TodoList = () => {
   return (
-    <>
+    <TodoListMainBox>
       <TodoBox />
-    </>
+    </TodoListMainBox>
   );
 };
 
