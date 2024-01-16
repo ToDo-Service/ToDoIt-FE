87687e8e4c1f6d559238b3f49eece2e2c9307195
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-01-16 12:46:49 +0900

    Feat: 메인 헤더 디자인

--- src/organisms/Header/index.tsx
+++ src/organisms/Header/index.tsx
@@ -4,12 +4,37 @@ const HeaderContainer = styled.div`
   width: 80vw;
   height: 13vh;
   border-bottom: solid 0.02px #c8c5cb;
+  display: flex;
+  align-items: center;
 `;
 
-const Header = () => {
+const HeaderTest = styled.h3`
+  font-family: "Pretendard";
+  font-size: 30px;
+  margin-left: 81px;
+  font-weight: 700;
+`;
+
+const HeaderLogin = styled.p`
+  font-family: "Pretendard";
+  font-size: 16px;
+  margin-left: 49vw;
+`;
+const HeaderSignUp = styled.p`
+  font-family: "Pretendard";
+  font-size: 16px;
+  margin-left: 14px;
+`;
+
+const Header = ({ Headername }: any) => {
+  console.log(Headername);
   return (
     <header>
-      <HeaderContainer>헤더입니다</HeaderContainer>
+      <HeaderContainer>
+        <HeaderTest>{Headername}</HeaderTest>
+        <HeaderLogin>로그인</HeaderLogin>
+        <HeaderSignUp>회원가입</HeaderSignUp>
+      </HeaderContainer>
     </header>
   );
 };
--- src/templates/PageTemp/index.tsx
+++ src/templates/PageTemp/index.tsx
@@ -1,10 +1,13 @@
 import Header from "@/organisms/Header";
 import TodoList from "@/organisms/\bTodoList";
+import { useState } from "react";
 
 const PageTemp = () => {
+  const [HeaderName, setHeaderName] = useState(["오늘의 할 일 "]);
+
   return (
     <div>
-      <Header />
+      <Header Headername={HeaderName[0]} />
       <TodoList />
     </div>
   );
