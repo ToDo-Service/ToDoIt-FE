00097e72b257c602596cb16febbbd76eb042367c
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-01-18 11:10:21 +0900

    Feat: google 로그인

--- src/organisms/Login/index.tsx
+++ src/organisms/Login/index.tsx
@@ -1,28 +1,28 @@
+import { InferGetServerSidePropsType } from "next";
 import { getCsrfToken, signIn, signOut, useSession } from "next-auth/react";
+import { getServerSideProps } from "next/dist/build/templates/pages";
+import { useSearchParams } from "next/navigation";
 
 const Login = () => {
   const { data: session } = useSession();
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "";
+
+  console.log(callbackUrl);
+
   console.log(session);
 
   const GetToken = async () => {
     const csrfToken = await getCsrfToken();
     console.log(csrfToken);
   };
+  GetToken(); //토큰 발급
 
-  GetToken();
-
-  if (session) {
-    return (
-      <>
-        {session.user?.name}님 반갑습니다 <br />
-        <button onClick={() => signOut()}>로그아웃</button>
-      </>
-    );
-  }
   return (
     <>
       로그인되지 않았습니다 <br />
-      <button onClick={() => signIn("kakao")}>Sign in</button>
+      <button onClick={() => signIn("kakao", { callbackUrl })}>kakao</button>
+      <button onClick={() => signIn("google")}>google</button>
     </>
   );
 };
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next";
 import KakaoProvider from "next-auth/providers/kakao";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
@@ -7,5 +8,14 @@ export default NextAuth({
       clientId: process.env.KAKAO_CLIENT_ID!,
       clientSecret: process.env.KAKAO_CLIENT_SECRET!,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
+
+  //호출 되었을 때 어디를 보여줄지 결정
+  //   pages: {
+
+  //   },
 });
