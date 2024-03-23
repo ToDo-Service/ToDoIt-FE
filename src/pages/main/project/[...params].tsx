a22667e0b983bb011c01dc213575d3f9978acc69
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-03-23 16:15:52 +0900

    Design: 로고 수정

--- src/pages/main/project/[...params].tsx
+++ src/pages/main/project/[...params].tsx
@@ -1,10 +1,18 @@
 import ProjectDetailLayout from "@/templates/ProjectDetailLayout";
 import Layout from "../layout";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <Layout>
-      <ProjectDetailLayout />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="Icon/Todoit/TodoitLogo.png" />
+        <title>TodoIt</title>
+      </Head>
+      <Layout>
+        <ProjectDetailLayout />
+      </Layout>
+    </>
   );
 }
