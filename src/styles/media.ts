826606fab75656db5b9c8b04fd24b0eefff2174f
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-04-08 14:00:36 +0900

    FIx: meadia 기기값 변경

--- src/styles/media.ts
+++ src/styles/media.ts
@@ -5,7 +5,7 @@ type DeviceType = "desktop" | "tablet" | "phone";
 const sizes: Record<DeviceType, number> = {
   desktop: 1440,
   tablet: 1024,
-  phone: 375,
+  phone: 390,
 };
 
 const media = Object.entries(sizes).reduce((acc, [key, value]) => {
