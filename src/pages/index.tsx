d72f6efc5a7c1c28fcfd07291587e1c635fbf8e8
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-04-21 15:28:37 +0900

    Feat: 구글검색엔진

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,10 @@ export default function Home() {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="google-site-verification"
+          content="R1ER2VRpZqX-xxD-GJdX6bF3uao9hRRAwmx9qPX4isE"
+        />
         <link rel="icon" href="Icon/Todoit/TodoitLogofavion.png" />
         <title>TodoIt</title>
       </Head>
