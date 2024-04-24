50db219df6509aeda0cbf34203c41b23f7c94199
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-04-25 08:46:39 +0900

    Feat: ProejctDetailMainPage 칸반 레이아웃

--- src/organisms/Project/ProjectDetail/ProjectDetailMainPage/index.tsx
+++ src/organisms/Project/ProjectDetail/ProjectDetailMainPage/index.tsx
@@ -9,6 +9,7 @@ import ProjectAdd from "@/molecules/PROJECT/ProjectAdd";
 import { FC, useState } from "react";
 import { ProejectTodoAdd } from "@/organisms/Project/ProjectDetail/ProjectTodoAddModal";
 import { media } from "@/styles/media";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 interface TodoItem {
   content: string;
@@ -24,7 +25,6 @@ const ProjectDetailMainPageBox = styled.div`
   height: 100%;
   padding-left: 345px;
   padding-top: 163px;
-  font-family: "Pretendard";
 
   ${media.phone`
   padding-left: 0;
@@ -32,8 +32,15 @@ const ProjectDetailMainPageBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
   `}
+
+  & .projectlists {
+    display: flex;
+
+    & .proejctitem:not(:first-child) {
+      margin-right: 24.72px;
+    }
+  }
 `;
 
 const ProjectDetailHeaderText = styled.h3`
@@ -41,8 +48,8 @@ const ProjectDetailHeaderText = styled.h3`
   position: sticky;
   height: max-content;
   top: 0;
-  padding-left: 5px;
-  padding-top: 5px;
+  font-family: "Pretendard-Bold";
+  font-weight: 300;
   margin-bottom: 40px;
 `;
 
@@ -58,6 +65,53 @@ const ProjectDetailList = styled.section`
     display: none;
   }
 `;
+
+const ProjectDetailBelongPersonList = styled.ul`
+  list-style: none;
+  display: flex;
+  width: max-content;
+  margin-left: 0;
+  padding-left: 0;
+
+  & li:not(:first-child) {
+    margin-left: 35px;
+  }
+`;
+
+const ProjectDetailBelongPerson = styled.li`
+  font-family: "PretendardVariable";
+  font-weight: 250;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 81ox;
+  min-width: 81px;
+  height: 40px;
+`;
+
+const ProjectDetailUserImg = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 30px;
+`;
+
+const ProjectDetailUserName = styled.p`
+  font-family: "PretendardVariable";
+  font-weight: 250;
+  font-size: 15px;
+  color: rgba(0, 0, 0, 0.8);
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+
+const ProejctDetailProgressLayout = styled.div`
+  max-width: 330px;
+  max-height: 550px;
+  width: 22.9167vw;
+  height: 53.7109vh;
+  background-color: rgba(12, 0, 24, 0.04);
+`;
+
 interface ProjectDetailProps {
   ProjectId: string;
 }
@@ -69,10 +123,23 @@ const ProjectDeatailMainPage: FC<ProjectDetailProps> = (props) => {
     (url: string) => Fetcher(url, JwtToken)
   );
 
+  const [UserList, setUserList] = useState([
+    { id: 1, name: "승연", img: "/Icon/Modal/ModalExit.png" },
+    { id: 2, name: "연준", img: "/Icon/Modal/ModalExit.png" },
+    { id: 3, name: "승찬", img: "/Icon/Modal/ModalExit.png" },
+  ]);
+
   const HeaderText: string = data?.body.project_info.title;
-  const TodoList: Array<TodoItem> = data?.body.todo_list;
+
   const setModal = useSetRecoilState(Modal);
-  const modal = useRecoilValue(Modal);
+
+  const handleChange = (result: any) => {
+    if (!result.destination) return;
+    const items = [...UserList];
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setUserList(items);
+  };
 
   const openAddModal = () => {
     setModal({ method: "post", toggle: true });
@@ -80,23 +147,53 @@ const ProjectDeatailMainPage: FC<ProjectDetailProps> = (props) => {
 
   return (
     <ProjectDetailMainPageBox>
-      <ProjectDetailList>
-        <ProjectDetailHeaderText>{HeaderText}</ProjectDetailHeaderText>
-        {TodoList &&
-          TodoList.map((item: TodoItem) => {
-            return <ProjectTodoBox todolist={item} />;
-          })}
-        <ProjectAdd
-          width="22.2222vw"
-          comment="+ 할 일을 추가하세요"
-          maxwidth="320px"
-          minwidth="320px"
-          onclick={openAddModal}
-        />
-      </ProjectDetailList>
-      {modal && props.ProjectId && openAddModal && (
-        <ProejectTodoAdd projectId={props.ProjectId} onclose={openAddModal} />
-      )}
+      <ProjectDetailHeaderText>{HeaderText}</ProjectDetailHeaderText>
+      <ProjectDetailBelongPersonList>
+        {UserList.map((user) => {
+          return (
+            <ProjectDetailBelongPerson>
+              <ProjectDetailUserImg src={user.img} alt="/" />
+              <ProjectDetailUserName>{user.name}</ProjectDetailUserName>
+            </ProjectDetailBelongPerson>
+          );
+        })}
+      </ProjectDetailBelongPersonList>
+      <DragDropContext onDragEnd={handleChange}>
+        <Droppable droppableId="projectlists">
+          {(provided) => (
+            <div
+              className="projectlists"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              <ProejctDetailProgressLayout>
+                {UserList.map((e: any, i: number) => {
+                  return (
+                    <Draggable
+                      draggableId={`test-${e.id}`}
+                      index={i}
+                      key={`test-${e.id}`}
+                    >
+                      {(provided, snapshot) => {
+                        return (
+                          <div
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            ref={provided.innerRef}
+                            className="proejctitem"
+                          >
+                            {e.name}
+                          </div>
+                        );
+                      }}
+                    </Draggable>
+                  );
+                })}
+              </ProejctDetailProgressLayout>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </ProjectDetailMainPageBox>
   );
 };
